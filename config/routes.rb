66b82d36413fe64d73d2f9e94ef355fb85627a61
Rafał Rybicki <rafarybicki@gmail.com>
2020-03-02 11:18:13 +0100

    Add Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create, :destroy, :index, :show, :update] do
+    resources :artworks, only: [:index]
+    resources :collections, only: [:index]
+  end
 end
